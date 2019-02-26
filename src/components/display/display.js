1517094cc31793236302ba65dfad08f940cae69e
Dmitry Lytov <38041284+Dmitrycmc@users.noreply.github.com>
2019-02-26 10:23:17 +0300

    feat: implemented displaying negatives total time (#2)

--- src/components/display/display.js
+++ src/components/display/display.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Icon from '../icon/icon';
 import { MODES } from '../device/device-modes';
-import { Time } from '../time/time';
+import { Time } from './time';
 import './display.css';
 
-export const Display = ({ current, total, showDelimiter, mode }) => (
-    <div className="display-wrapper">
-        <span className="indicators-total-wrapper">
-            <span className="indicators-wrapper">
-                <Icon type="play" inactive={mode != MODES.play} />
-                <Icon type="pause" inactive={mode != MODES.pause} />
-                <Icon type="stop" inactive={mode != MODES.stop} />
+export const Display = ({ current, total, showDelimiter, mode }) => {
+    const negative = total < 0;
+
+    return (
+        <div className="display-wrapper">
+            <span className="indicators-total-wrapper">
+                <span className="indicators-wrapper">
+                    <Icon type="play" inactive={mode != MODES.play} />
+                    <Icon type="pause" inactive={mode != MODES.pause} />
+                    <Icon type="stop" inactive={mode != MODES.stop} />
+                </span>
+                <span className={!negative ? "inactive" : ""}>-</span>
+                <Time delimiter hideSeconds className="display-total" value={negative ? -total : total} />
             </span>
-            <Time delimiter hideSeconds className="display-total" value={total} />
-        </span>
-        <Time delimiter={showDelimiter} className="display-current" value={current} />
-    </div>
-);
+            <Time delimiter={showDelimiter} className="display-current" value={current} />
+        </div>
+    );
+}
