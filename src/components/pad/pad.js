2588ec01d8f064923588c4981a0e1da609abd216
Dmitry Lytov <dmitrycmc@gmail.com>
2019-03-04 10:03:06 +0300

    works, need optimize

--- src/components/pad/pad.js
+++ src/components/pad/pad.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Sound from 'react-sound';
+import soundfile from '../../assets/sounds/click1.mp3';
 import './pad.css';
 
 const PadTemplate = ({ onClick, children, className }) => {
@@ -13,8 +15,20 @@ class PadWithKey extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pressed: false
+            pressed: false,
+            status: Sound.status.STOPPED
         };
+    }
+
+    handleMouseDown = e => {
+        console.log(e);
+        this.setState({ pressed: true, status: Sound.status.PLAYING });
+    }
+
+    handleMouseUp = e => {
+        if (!this.state.pressed) return;
+        this.setState({ pressed: false });
+        this.props.onClick();
     }
 
     handleKeyDown = e => {
@@ -22,7 +36,7 @@ class PadWithKey extends Component {
         const { pressed } = this.state;
 
         if (e.keyCode !== keyCode || pressed) return;
-        this.setState({ pressed: true });
+        this.setState({ pressed: true, status: Sound.status.PLAYING });
     };
 
     handleKeyUp = e => {
@@ -32,21 +46,33 @@ class PadWithKey extends Component {
         onClick && onClick();
     };
 
+    shouldComponentUpdate(nextProps, nextState){
+        return this.state.pressed != nextState.pressed;
+    }
+
     componentDidMount() {
         document.addEventListener('keydown', this.handleKeyDown);
         document.addEventListener('keyup', this.handleKeyUp);
+        document.addEventListener('mouseup', this.handleMouseUp);
     }
 
     componentWillUnmount() {
         document.removeEventListener('keydown', this.handleKeyDown);
         document.removeEventListener('keyup', this.handleKeyUp);
+        document.removeEventListener('mouseup', this.handleMouseUp);
     }
 
     render() {
-        const { onClick, children, className } = this.props;
+        const { children, className } = this.props;
         const { pressed } = this.state;
+        console.log(this.state.pressed);
         return (
-            <PadTemplate onClick={onClick} children={children} className={`${className} ${pressed ? 'pressed' : 0}`} />
+            <div onMouseDown={this.handleMouseDown}>
+                <Sound url={soundfile}
+                       playStatus={this.state.status} loop={false}
+                />
+                <PadTemplate children={children} className={`${className} ${pressed ? 'pressed' : ''}`} />
+            </div>
         );
     }
 }
