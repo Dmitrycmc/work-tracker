52ce691c8b485760a3710d66aff4f44e0d128f93
Dmitry Lytov <dmitrycmc@gmail.com>
2019-04-22 15:18:02 +0300

    Updates

---
+++ precache-manifest.3e26be10cda77d97dca46f1c0b505622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/work-tracker/static/media/digital-7 (mono).58045dab.ttf"
+  },
+  {
+    "revision": "e1fede7a55b1c2ca66fd",
+    "url": "/work-tracker/static/js/runtime~main.e46659b8.js"
+  },
+  {
+    "revision": "028247f06db1aa6f260e",
+    "url": "/work-tracker/static/js/main.ada7c72c.chunk.js"
+  },
+  {
+    "revision": "b83e04106de25ec4f038",
+    "url": "/work-tracker/static/js/2.57e63307.chunk.js"
+  },
+  {
+    "revision": "028247f06db1aa6f260e",
+    "url": "/work-tracker/static/css/main.758d682c.chunk.css"
+  },
+  {
+    "revision": "3fcbe990352038c70dfe99475cc457cc",
+    "url": "/work-tracker/index.html"
+  }
+];
